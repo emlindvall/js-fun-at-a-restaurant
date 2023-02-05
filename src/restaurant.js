d31f18d3d32104922f27ac96eedddb634d904aa8
Em Lindvall <elindvall22@turing.edu>
2023-02-04 18:04:16 -0700

    Revise code for the addMenuItem and the removeMenuItem functions in restaurants.js file

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,48 +10,34 @@ function createRestaurant(name) {
   return pizzaRestaurant;
 }
 
+// Soooooo I could NOT stop thinking about this and I let it bother me until I figured out
+// a better way to code both the addMenuItem and the removeMenuItem functions to be a lot 
+// cleaner and less horrifying. Enjoy:
+
 function addMenuItem(pizzaRestaurant, menuItem)  {
-  if (menuItem.type === "breakfast" && !pizzaRestaurant.menus.breakfast.includes(menuItem))  {
-    pizzaRestaurant.menus.breakfast.push(menuItem);
-  } if (menuItem.type === "lunch" && !pizzaRestaurant.menus.lunch.includes(menuItem))  {
-    pizzaRestaurant.menus.lunch.push(menuItem);
-  } else if (menuItem.type === "dinner" && !pizzaRestaurant.menus.dinner.includes(menuItem)){
-    pizzaRestaurant.menus.dinner.push(menuItem);
-  }
+  var itemType = menuItem.type;
+  if (!pizzaRestaurant.menus[itemType].includes(menuItem))  {
+    pizzaRestaurant.menus[itemType].push(menuItem);
   return pizzaRestaurant;
+  }
 }
 
-// This is about to be sooooo gnarly. Brace yourself.
-
 function removeMenuItem(pizzaRestaurant, menuItem, itemType) {
-  if (itemType === "breakfast" && pizzaRestaurant.menus.breakfast.length > 0) {
-    for (var i = 0; i <= pizzaRestaurant.menus.breakfast.length; i++) {
-      if (pizzaRestaurant.menus.breakfast[i].name === menuItem) {
-        pizzaRestaurant.menus.breakfast.splice(i, 1);
+  if (pizzaRestaurant.menus[itemType].length > 0) {
+    for (var i = 0; i <= pizzaRestaurant.menus[itemType].length; i++) {
+      if (pizzaRestaurant.menus[itemType][i].name === menuItem) {
+        pizzaRestaurant.menus[itemType].splice(i, 1);
         var nobodyEating = (`No one is eating our ${menuItem} - it has been removed from the ${itemType} menu!`);
         return nobodyEating
        }
-      }
-  } else if (itemType === "lunch" && pizzaRestaurant.menus.lunch.length > 0) {
-      for (var i = 0; i <= pizzaRestaurant.menus.lunch.length; i++) {
-        if (pizzaRestaurant.menus.lunch[i].name === menuItem) {
-          pizzaRestaurant.menus.lunch.splice(i, 1);
-          var nobodyEating = (`No one is eating our ${menuItem} - it has been removed from the ${itemType} menu!`);
-          return nobodyEating
-        }
-      }
-  } else if (itemType === "dinner" && pizzaRestaurant.menus.dinner.length > 0) {
-      for (var i = 0; i <= pizzaRestaurant.menus.dinner.length; i++) {
-        if (pizzaRestaurant.menus.dinner[i].name === menuItem) {
-          pizzaRestaurant.menus.dinner.splice(i, 1);
-          var nobodyEating = (`No one is eating our ${menuItem} - it has been removed from the ${itemType} menu!`);
-          return nobodyEating
-        }
-      }
-  }
+    }
+  } else {
   var weDontSellIt = (`Sorry, we don't sell ${menuItem}, try adding a new recipe!`);
   return weDontSellIt;
+  }
 }
+
+
 
 module.exports = {
   createRestaurant, 
